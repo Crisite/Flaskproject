9f391e4c328e638dc6e601136ef751dbaea74b6c
crisite <844858692@qq.com>
2021-07-01 20:49:48 +0800

    r

---
+++ app/models.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# from run import app
+from app import DevelopmentConfig
+
+app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
+db = SQLAlchemy(app)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+
+
+# 删除表
+# db.drop_all()
+
+# 创建表
+# db.create_all()
